a2d4165b6034d161887b48ddb5211b00da58ab92
Tonyratnam162 <gudimetlatony@gmail.com>
2024-03-05 18:38:10 +0530

    all files are created

---
+++ src/main/java/tony/penny/PennyApplication.java
@@ -0,0 +1,13 @@
+package tony.penny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tony/penny/PennyApplicationTests.java
@@ -0,0 +1,13 @@
+package tony.penny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
